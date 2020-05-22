e76c755d031b2d5364aa69ebb52a188b0688f255
“stefanhans” <uni@stefan-hans.de>
2020-05-22 09:50:55 +0200

    test for storage locally executed

--- functions/e2e-storage_test.go
+++ functions/e2e-storage_test.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"firebase.google.com/go"
-	"fmt"
 	"google.golang.org/api/option"
 	"io/ioutil"
 	"net/http"
@@ -47,24 +46,13 @@ func TestE2eStorage(t *testing.T) {
 		t.Fatalf("failed to return default bucket handle: %v\n", err)
 	}
 
-	ctx, cancel := context.WithTimeout(ctx, time.Second*50)
-	defer cancel()
-
 	var testCases map[string]struct {
-		request              Request
-		response             Response
-		expectedError        bool
-		expectedStatusCode   int
-		expectedStatusPrefix string
+		request Request
 	}
 	testCases = map[string]struct {
-		request              Request
-		response             Response
-		expectedError        bool
-		expectedStatusCode   int
-		expectedStatusPrefix string
+		request Request
 	}{
-		"Standard": {
+		"Storage v0.0.1": {
 			request: Request{
 				ClientVersion:  "0.0.1",
 				ClientId:       "beab10c6-deee-4843-9757-719566214526",
@@ -72,10 +60,6 @@ func TestE2eStorage(t *testing.T) {
 				SourceLanguage: "en",
 				TargetLanguage: "de",
 			},
-			response: Response{
-				TranslatedText: "Heute ist die Himmelfahrt Jesu",
-			},
-			expectedError: false,
 		},
 	}
 	for n, tc := range testCases {
@@ -97,82 +81,76 @@ func TestE2eStorage(t *testing.T) {
 				t.Errorf("failed to send POST request to %q: %v\n", serviceUrl, err)
 				return
 			}
-
 			body, err := ioutil.ReadAll(res.Body)
-			fmt.Printf("body: %v\n", string(body))
+			//fmt.Printf("body: %v\n", string(body))
 
-			if tc.expectedError {
-
-				fmt.Printf("Status: %v\n", res.Status)
-				fmt.Printf("Body: %s\n", body)
-
-				if res.StatusCode != tc.expectedStatusCode {
-					t.Errorf("status code is %v and not as expected %v\n", res.StatusCode, tc.expectedStatusCode)
-					return
-				}
-				if strings.HasPrefix(res.Status, tc.expectedStatusPrefix) {
-					t.Errorf("status text is %v and has not as expected the prefix %v\n", res.StatusCode, tc.expectedStatusCode)
-					return
-				}
-			} else {
-
-				var response *Response
-				err = json.Unmarshal(body, &response)
-				if err != nil {
-					t.Errorf("failed to unmarshal response: %v\n", err)
-					return
-				}
-
-				fmt.Printf("TaskId: %s\n", response.TaskId)
-
-				if response.TaskId == "" {
-					t.Errorf("TaskId in response is empty\n")
-				}
-
-				if response.TranslatedText == "" {
-					t.Errorf("TranslatedText in response is empty\n")
-				}
-
-				if response.TranslatedText != tc.response.TranslatedText {
-					t.Errorf("TranslatedText is %q and not as expected %q\n", response.TranslatedText, tc.response.TranslatedText)
-				}
-
-				if response.LoadCommand == "" {
-					t.Errorf("LoadCommand in response is empty\n")
-				}
-
-				//response.TaskId = "d09870fc-8e3e-4ec6-9808-501155e94915"
-				time.Sleep(time.Second * 2)
-
-				var translationTaskJson []byte
-				rc, err := bucket.Object(tc.request.ClientVersion + "/" + response.TaskId).NewReader(ctx)
-				if err != nil {
-					t.Errorf("failed to create reader %q\n", err)
-					return
-				} else {
-					_, err = rc.Read(translationTaskJson)
-					if err != nil {
-						t.Errorf("failed to read %q\n", err)
-						return
-					}
-				}
-
-				fmt.Printf("translationTaskJson: %s\n", translationTaskJson)
-				fmt.Printf("translationTaskJson: %s\n", &translationTaskJson)
-
-				var translationTask TranslationTask
-				err = json.Unmarshal(translationTaskJson, &translationTask)
-				if err != nil {
-					t.Errorf("failed to unmarshal translationTask: %v\n", err)
-					return
-				}
-				fmt.Printf("translationTask.TaskId: %v\n", translationTask.TaskId)
-
+			var response *Response
+			err = json.Unmarshal(body, &response)
+			if err != nil {
+				t.Errorf("failed to unmarshal response: %v\n", err)
+				return
 			}
+			//fmt.Printf("TaskId: %s\n", response.TaskId)
+			//fmt.Printf("gsutil cat gs://%s/%s/%s\n", bucketUrl, tc.request.ClientVersion, response.TaskId)
 
 			err = res.Body.Close()
 			if err != nil {
 				t.Errorf("cannot close response body\n")
+			}
+
+			time.Sleep(time.Second * 2)
+
+			ctx, cancel := context.WithTimeout(ctx, time.Second*50)
+			defer cancel()
+
+			var translationTaskJson []byte
+			rc, err := bucket.Object(tc.request.ClientVersion + "/" + response.TaskId).NewReader(ctx)
+			if err != nil {
+				t.Errorf("failed to create reader %q\n", err)
+				return
+			} else {
+				translationTaskJson, err = ioutil.ReadAll(rc)
+				if err != nil {
+					t.Errorf("failed to read %q\n", err)
+					return
+				}
+			}
+			//fmt.Printf("translationTaskJson: %s\n", translationTaskJson)
+
+			err = rc.Close()
+			if err != nil {
+				t.Errorf("cannot close reader\n")
+			}
+
+			var translationTask TranslationTask
+			err = json.Unmarshal(translationTaskJson, &translationTask)
+			if err != nil {
+				t.Errorf("failed to unmarshal translationTask: %v\n", err)
+				return
+			}
+
+			if translationTask.ClientVersion != tc.request.ClientVersion {
+				t.Errorf("ClientVersion is %q and not as expected %q\n", translationTask.ClientVersion, tc.request.ClientVersion)
+			}
+			if translationTask.ClientId != tc.request.ClientId {
+				t.Errorf("ClientId is %q and not as expected %q\n", translationTask.ClientId, tc.request.ClientId)
+			}
+			if translationTask.Text != tc.request.Text {
+				t.Errorf("Text is %q and not as expected %q\n", translationTask.Text, tc.request.Text)
+			}
+			if translationTask.SourceLanguage != tc.request.SourceLanguage {
+				t.Errorf("SourceLanguage is %q and not as expected %q\n", translationTask.SourceLanguage, tc.request.SourceLanguage)
+			}
+			if translationTask.TargetLanguage != tc.request.TargetLanguage {
+				t.Errorf("TargetLanguage is %q and not as expected %q\n", translationTask.TargetLanguage, tc.request.TargetLanguage)
+			}
+
+			if translationTask.TaskId != response.TaskId {
+				t.Errorf("TaskId is %q and not as expected %q\n", translationTask.TaskId, response.TaskId)
+			}
+
+			if translationTask.TranslatedText != response.TranslatedText {
+				t.Errorf("TranslatedText is %q and not as expected %q\n", translationTask.TranslatedText, response.TranslatedText)
 			}
 		})
 	}
